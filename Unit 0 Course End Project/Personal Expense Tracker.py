cf9c6612f7547797b9712f9eaaa0417043e40832
ChukwukaEjinkonye-jpg <chukwukaejinkonye@gmail.com>
2025-03-31 17:24:59 -0400

    Blueprint Personal Expense Tracker

---
+++ Unit 0 Course End Project/Personal Expense Tracker.py
@@ -0,0 +1,39 @@
+#Libraries
+#Look into potential Python GUI libraries
+
+
+expenses = []
+months = {}
+
+def addExpense():
+    #Date format YYYY-MM-DD
+    #Expense Category (Food, Travel, etc)
+    #Amount Spent 
+    #Brief Description
+    #Ex: {'date': '2024-09-18', 'category': 'Food', 'amount': 15.50, 'description': 'Lunch with friends'}
+    #add above dict to 'expenses' array
+    #Inform the user how much is left from the monthly budget after each input
+    print("lorem ipsum")
+
+def viewExpense():
+    #Iterate through 'expenses' array and display the date, category, amount, and description for each entry
+    #Skip each entry that have any missing details and inform user which ones are incomplete afterwards
+    print("lorem ipsum")
+
+def setBudget():
+    #Allow for the user to input and set a monthly budget
+    print("lorem ipsum")
+
+def trackBudget():
+    #Calculates the total expenses recorded so far 
+    #If the total expenses exceed the budget, display a warning (Example: You have exceeded your budget!)
+    #If the expenses are within the budget, display the remaining balance (Example: You have 150 left for the month)
+    print("lorem ipsum")
+
+def saveCSV():
+    #Save every entry in the expenses array into a CSV file (Date , Category , Amount , Description)
+    print("lorem ipsum")
+
+def loadCSV():
+    #Reads saved CSV file and uploads data to user is GUI
+    print("lorem ipsum")
