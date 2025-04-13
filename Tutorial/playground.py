be82bad9679d19239b80be6c9b8dc52c08d7061d
ChukwukaEjinkonye-jpg <chukwukaejinkonye@gmail.com>
2025-04-13 19:27:35 -0400

    Expense Tracker MVP done

--- Tutorial/playground.py
+++ Tutorial/playground.py
@@ -1,3 +1,5 @@
-list = "turbo"
-print(list)
+first = [{'Chicken' : {'Brown' : 39 , "White" : 4 , "Red" : 53 , "Black" : 61}}, {'Cow' : {'Brown' : 11 , "White" : 43 , "Red" : 4 , "Black" : 500}}]
 
+
+second = [{"Bob_farm" : [{'Chicken' : {'Brown' : 39 , "White" : 4 , "Red" : 53 , "Black" : 61}}, {'Cow' : {'Brown' : 11 , "White" : 43 , "Red" : 4 , "Black" : 500}}] } 
+          , {"George Farm" : [{'Chicken' : {'Brown' : 23 , "White" : 4 , "Red" : 44 , "Black" : 90}}, {'Cow' : {'Brown' : 13 , "White" : 43 , "Red" : 19 , "Black" : 5}}]}]
--- Unit 0 Course End Project/Personal Expense Tracker.py
+++ Unit 0 Course End Project/Personal Expense Tracker.py
@@ -1,10 +1,30 @@
 #Libraries
 #Look into potential Python GUI libraries
-import calendar
-import re
+import calendar , csv , os , re
+import pandas as pd
 
-expenses = []
-budget = 1000
+filepath = "tracker_save_file.csv"
+expenses = [{'date' : '2024-04-20' , 'category' : 'Weed' , 'amount' : 90 , 'description' : 'Smoke weed erryday'} , 
+            {'date' : '2025-04-01' , 'category' : 'Food' , 'amount' : 20 , 'description' : 'Chinese take-out'} ,
+            {'date' : '2024-12-25' , 'category' : 'Holiday' , 'amount' : 2000 , 'description' : 'Holiday Shopping'} ,
+            {'date' : '2002-09-11' , 'category' : 'Holiday' , 'amount' : 100 , 'description' : 'B-day'} ,
+            {'date' : '2020-07-04' , 'category' : 'Travel' , 'amount' : 70 , 'description' : 'Amtrack fare'} ]
+header = ['Date' , 'Category' , 'Amount' , 'Description']
+budget = 5000
+
+
+def initial():
+    if not os.path.exists(filepath):  #Creates new save file at start
+        with open(filepath, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(header)
+        print(f"'{filepath}' created successfully.")
+    else:
+        print(f"'{filepath}' already exists.")
+
+def setBudget(value):
+    budget = round(value , 2)
+    print(f"Monthly budget is now set to: ${budget}")
 
 def get_valid_month():
     while True:
@@ -71,52 +91,92 @@ def addExpense():
 
     #Extracting brief description data 
     purchase_description = input("Briefly explain the purpose of the purchase: ")
-
-    try:
-        if budget - money_amount >= 0:
-            print(f"Purchase recorded. You have ${budget - money_amount} left this month.")
-        else:
-            print(f"Purchase recorded. It looks like you have exceeded your monthy budget by ${abs(budget - money_amount)}")
-    except:
-        print("Purchase recorded.")
+    
+    print("Purchase recorded.")
 
     expense = {'date': date , 'category':category , 'amount':money_amount , 'description':purchase_description}
     expenses.append(expense)
  
-
-def viewExpense():
+def viewExpenses():
     #Iterate through 'expenses' array and display the date, category, amount, and description for each entry
     #Skip each entry that have any missing details and inform user which ones are incomplete afterwards
-    print("lorem ipsum")
     missing_details = []
     for expense in expenses:
-        if any(not bool(value) for value in expense.values()):
+        if any(not bool(value) for value in expense.values()): #Check if expense entry has any empty values. Skip if so
             missing_details.append(expense)
         else:
             date = expense['date'].split('-')
-            print(f"Date: {list(calendar.month_name)[int(date[1])]} {date[2]}, {date[0]} | Category: {expense['category']} | Amount: ${expense['amount']} | Description: {expense['expense']}")
+            print(f"Date: {list(calendar.month_name)[int(date[1])]} {date[2]}, {date[0]} | Category: {expense['category']} | Amount: ${expense['amount']} | Description: {expense['description']}") #Print out expense entry for user
     if len(missing_details) > 0:
         print("Some entries contain missing information")
 
+def trackBudget(): 
+    try:
+        data = pd.read_csv(filepath)
+        total_expenses = data['Amount'].sum()
+        print(f'You have spent ${total_expenses} in total this month.')
+        
+        
+        if total_expenses > budget: # If user goes over budget 
+            print("You have exceeded your monthly budget!")
+        elif total_expenses <= budget: # If user is still within budget
+            print(f'You have ${budget - total_expenses} left for the month.')
 
-def setBudget(value):
-    budget = value
-    print(f"Monthly budget is now set to: {budget}")
-
-def trackBudget():
-    #Calculates the total expenses recorded so far 
-    #If the total expenses exceed the budget, display a warning (Example: You have exceeded your budget!)
-    #If the expenses are within the budget, display the remaining balance (Example: You have 150 left for the month)
-    print("lorem ipsum")
+    except FileNotFoundError:
+        print("Save file not found. Creating new save file")
+        initial()
 
 def saveCSV():
-    #Save every entry in the expenses array into a CSV file (Date , Category , Amount , Description)
-    print("lorem ipsum")
+    global expenses
+
+    with open(filepath, mode='a' , newline= '', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=header)
+        
+        for expense in expenses:  #Appends every entry in the expenses array into a CSV file
+            writer.writerow({
+            'Date': expense['date'],
+            'Category': expense['category'],
+            'Amount': expense['amount'],
+            'Description': expense['description']
+        })
+            
+    expenses = []  #Resets expenses array so that old values don't get appended twice
+    print("\nExpenses saved successfully.\n")
 
 def loadCSV():
-    #Reads saved CSV file and uploads data to user is GUI
-    print("lorem ipsum")
+    #Reads saved CSV file and uploads data to user in GUI
+    missing_details = []
+    sorted_data = pd.read_csv(filepath).sort_values(by='Date')
 
-addExpense()
+    for index, row in sorted_data.iterrows():
+        if row.isnull().any():
+            missing_details.append(f"Date: {row.get('Date', '')}, Category: {row.get('Category', '')}, Amount: {row.get('Amount', '')}, Description: {row.get('Description', '')}")
+        else:
+            print(f"Date: {row['Date']}, Category: {row['Category']}, Amount: {row['Amount']}, Description: {row['Description']}")
+
+    if len(missing_details) > 0:
+        print("!!!Warning!!! \nSome entries are missing critical information")
+
+        for entry in missing_details:
+            print(entry)
+
+def menu():
+    while True:
+        option = input("What would you like to do?\n\n>Press '1' to add an expense\n>Press '2' to view expenses\n>Press '3' to track a monthly budget\n>Press '4' to save your expenses\n>Press '5' to quit")
+        if option not in ["1" , "2" , "3" , "4" , "5"]:
+            print("\nThat is not an option.\nPlease try again\n")
+        elif option == "5":
+            break
+        elif option == "1":
+            addExpense()
+        elif option == "2":
+            loadCSV()
+        elif option == "3":
+            trackBudget()
+        elif option == "4":
+            saveCSV()
+        
 
 
+initial()
+menu()
